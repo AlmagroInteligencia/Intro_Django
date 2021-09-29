9fdf8dc397bdd6c4d07b9eb63727497071ca949b
Marcos Pyznar <pyznar.marcos@gmail.com>
2021-09-28 23:59:26 -0300

    pequeña correcion

--- core/settings.py
+++ core/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 #TAILWIND_APP_NAME='theme'
 
 #INTERNAL_IPS = [
-    "127.0.0.1",
-]
+#    "127.0.0.1",
+#]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
